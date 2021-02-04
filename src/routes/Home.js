81a4ce379ae6b02b54ad7984905df7f1ff68e9a1
pyrata <jsoa6@hotmail.com>
2021-02-03 21:12:14 -0500

    pc v1.0

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,15 @@
-export default () => 'Home';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_MOVIES = gql`
+	query {
+		movies {
+			id
+			medium_cover_image
+		}
+	}
+`;
+export default () => {
+	const { loading } = useQuery;
+	console.log(loading);
+	return <h1>Home</h1>;
+};
